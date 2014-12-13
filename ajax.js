373d30a7e0ec898cbf4c679caa359edeece1ac7e
digital planets <success@dpsw.info>
2014-12-12 21:00:20 -0600

    Update ajax.js

--- ajax.js
+++ ajax.js
@@ -2,7 +2,6 @@ var AJAX = {};
 
 (function (AJAX) {
 
-
   var json2qs = function(o) {
     if (typeof o !== 'object') return o;
     var a = [];
@@ -12,24 +11,34 @@ var AJAX = {};
   
   AJAX.method = function(verb, u, d, cb) {
     var XMLHttpRequest = typeof window !== 'undefined' ? window.XMLHttpRequest : require('xhr2');
+
     var x = new XMLHttpRequest;
+
     if (verb == 'GET') u = u + '?' + json2qs(d);
+
     x.open(verb, u, true);
+
     if (verb != 'GET') {
       x.setRequestHeader('accept', 'application/json');
       x.setRequestHeader('Content-type', 'application/json');
       if (typeof d === 'object') d = JSON.stringify(d);
     }
-    x.send(d);
+
+    try{
+      x.send(d);
+    } catch(e){
+      cb('AJAX.js network error cannot connect');
+    }
+
     x.onload = function(r) {
       var resp = r.target.responseText;
       try {
         resp = JSON.parse(resp);
       } catch (e) {}
-
-      var err = r.target.status.toString()[0] !== "2";
-      (cb || console.log.bind(console))(err, r.target, resp);
+      var status = r.target.status;
+      (cb || console.log.bind(console))(status.toString()[0] == "2" ? null : status, r.target, resp);
     };
+    
   }
 
   AJAX.get = AJAX.method.bind(this, 'GET');
