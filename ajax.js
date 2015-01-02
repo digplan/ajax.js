46370783dba796dbf87a484dcf5e29aacb0b3bc0
digital planets <success@dpsw.info>
2015-01-02 17:54:09 -0600

    Update ajax.js

--- ajax.js
+++ ajax.js
@@ -9,56 +9,32 @@ var AJAX = {};
     return a.join('&');
   }
   
-  AJAX.method = function(verb, u, d, cb) {
+  AJAX.method = function(verb, u, d, cb, headers) {
     var XMLHttpRequest = typeof window !== 'undefined' ? window.XMLHttpRequest : require('xhr2');
-
     var x = new XMLHttpRequest;
-
-    if (verb == 'GET') u = u + '?' + json2qs(d);
-
+    if (verb == 'GET' && d && Object.keys(d).length) 
+      u = u + '?' + json2qs(d);
     x.open(verb, u, true);
-
-    if (verb != 'GET') {
-      x.setRequestHeader('accept', 'application/json');
-      x.setRequestHeader('Content-type', 'application/json');
-      if (typeof d === 'object') d = JSON.stringify(d);
-    }
-
-    try {
-      x.send(d);
-    } catch(e){
-      cb('AJAX.js network error cannot connect');
-    }
-
+    if (verb != 'GET' && typeof d === 'object') 
+      d = JSON.stringify(d);
+    if(headers)
+      for(k in headers) x.setRequestHeader(k, headers[k]);
+    x.send(d);
     x.onload = function(r) {
       var resp = r.target.responseText;
       try {
         resp = JSON.parse(resp);
-      } catch (e) {
+      } catch (e) {}
 
-      }
-      var status = r.target.status;
-      (cb || console.log.bind(console))(status.toString()[0] == "2" ? null : status, r.target, resp);
+      var err = r.target.status.toString()[0] !== "2";
+      (cb || console.log.bind(console))(resp);
     };
-    
   }
 
   AJAX.get = AJAX.method.bind(this, 'GET');
   AJAX.head = AJAX.method.bind(this, 'HEAD');
   AJAX.post = AJAX.method.bind(this, 'POST');
   AJAX.put = AJAX.method.bind(this, 'PUT');
-  AJAX.delete = AJAX.method.bind(this, 'DELETE');
-
-  if(typeof window === 'undefined') return;
-
-  AJAX.load = function(src, cb){
-    var css = s.match(/css$/);
-    var i = document.body.appendChild(document.createElement(css ? 'link' : 'script'));
-    i.onload = cb;
-    if (css)
-      css.rel = "stylesheet";
-    i[css ? 'href' : 'src'] = src;
-    css && cb && cb();
-  }
-
+  AJAX.delete = AJAX.method.bind(this, 'DELETE'); 
+  
 })(typeof exports !== 'undefined' ? exports : AJAX);
