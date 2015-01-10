33ef2c83e25ab7c1369147ca0d7fa02af912033f
digital planets <success@dpsw.info>
2015-01-10 14:47:43 -0600

    Update ajax.js

--- ajax.js
+++ ajax.js
@@ -2,23 +2,25 @@ var AJAX = {};
 
 (function (AJAX) {
 
-  var json2qs = function(o) {
-    if (typeof o !== 'object') return o;
-    var a = [];
-    for (i in o) a.push(i + '=' + o[i]);
-    return a.join('&');
-  }
-  
+  if(exports && !this.EventSource)
+    EventSource = require('eventsource');
+
   AJAX.method = function(verb, u, d, cb, headers) {
     var XMLHttpRequest = typeof window !== 'undefined' ? window.XMLHttpRequest : require('xhr2');
     var x = new XMLHttpRequest;
-    if (verb == 'GET' && d && Object.keys(d).length) 
-      u = u + '?' + json2qs(d);
     x.open(verb, u, true);
-    if (verb != 'GET' && typeof d === 'object') 
+
+    if (typeof d === 'object') 
       d = JSON.stringify(d);
+
+    if(d && d[0] !== '{')
+      x.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+
     if(headers)
       for(k in headers) x.setRequestHeader(k, headers[k]);
+
+    console.log('ajax.body', verb, d)
+
     x.send(d);
     x.onload = function(r) {
       var resp = r.target.responseText;
@@ -29,6 +31,7 @@ var AJAX = {};
       var err = r.target.status.toString()[0] !== "2";
       (cb || console.log.bind(console))(resp);
     };
+
   }
 
   AJAX.get = AJAX.method.bind(this, 'GET');
@@ -36,5 +39,13 @@ var AJAX = {};
   AJAX.post = AJAX.method.bind(this, 'POST');
   AJAX.put = AJAX.method.bind(this, 'PUT');
   AJAX.delete = AJAX.method.bind(this, 'DELETE'); 
-  
+  AJAX.events = function(endpoint, cb){
+    var e = new EventSource(endpoint);
+    e.onmessage = function(ev){
+      (cb||console.log.bind(console))(ev.data);
+    }
+    return {
+      close: e.close
+    }
+  }
 })(typeof exports !== 'undefined' ? exports : AJAX);
