86b10791615363697bc4b4d2129b1f16222fb688
unknown <chris.borkert@HHTHD12.alb.eresources.com>
2015-06-21 14:09:41 -0500

    minor fix

--- test.js
+++ test.js
@@ -4,6 +4,7 @@ var baseURI = 'http://http-echo.com';
 AJAX.get(baseURI, {}, function(e, r, b){
   console.log(e, r, b);
 });
+
 /*
 AJAX.head(baseURI, {}, function(e, r, b){
   console.log(r._headers);
